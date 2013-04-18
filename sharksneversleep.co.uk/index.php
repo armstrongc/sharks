3368cb90f5aaff8139405832f87c63ae52790f64
unknown <ArmstrongC@CAMECOMM847.uk.fc.local>
2013-04-18 17:57:06 +0100

    Home page updates

--- sharksneversleep.co.uk/index.php
+++ sharksneversleep.co.uk/index.php
@@ -25,24 +25,24 @@ require_once 'common/header_start.php';
     <!-- LEFT COLUMN -->
     <div class="grid_4">
         <div class="content_padding">
-            <h2>Front End Web Developer</h2>
-            <p>I'm a front end website developer, it's what i do. Sharks Never Sleep is a <a href="/blog/">web design and development blog</a> covering all aspects of my work.</p>
+            <h2>Sharks Never Sleep</h2>
+            <p>Sharks Never Sleep is a <a href="/blog/">web design and development blog</a> full to the brim of dazzling thoughts on Website Development.</p>
         </div>
     </div>
 
     <!-- CENTRE COLUMN COLUMN -->
     <div class="grid_4">
         <div class="content_padding">
-            <h2>Snippets, Examples &amp; Tutorials</h2>
-            <p>The posts on this site will cover and document a number of areas from <a href="/blog/css/">CSS</a>, <a href="/blog/javascript/">Javascript</a> and <a href="/blog/html/">HTML</a> to <a href="/blog/git/">GIT</a> and <a href="/blog/design/">websign design</a>.</p>
+            <h2>Guides &amp; Tutorials</h2>
+            <p>A whole host of tips, tricks and guides for front end website developers, including <a href="/blog/css/">CSS tutorials</a>, <a href="/blog/javascript/">Javascript tutorials</a>, <a href="/blog/html/">HTML tutorials</a> and <a href="/blog/git/">a guide to installing and getting started with GIT</a>.</p>
         </div>
     </div>
 
     <!-- RIGHT COLUMN -->
     <div class="grid_4">
         <div class="content_padding">
-            <h2>Responsive Website Design</h2>
-            <p>RWD is becoming a large part of any front end web developers workload. Hopefully I can make sense of some of the murkier areas.</p>
+            <h2>Push Things Forward</h2>
+            <p>Take advantage of the tools available to improve, innovate and excite on a daily basis. CSS3, HTML5, and Javascript are my tools, let's see what can be done with them.</p>
         </div>
     </div>
 
